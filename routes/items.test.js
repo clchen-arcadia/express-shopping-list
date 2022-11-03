623b04050228885cb675d28012bcbd8dbc44905d
Joel Alcaraz <joelchristopheralcaraz@gmail.com>
2022-11-02 17:53:35 -0700

    Add patch test route

--- routes/items.test.js
+++ routes/items.test.js
@@ -49,14 +49,14 @@ describe("test POST /items", function() {
         name: 'test',
         price: 'string'
       });
-      expect(resp.statusCode).toEqual(400);
+    expect(resp.statusCode).toEqual(400);
   });
 
   it("rejects empty body post requests", async function() {
     const resp = await request(app)
       .post('/items')
       .send({});
-      expect(resp.statusCode).toEqual(400);
+    expect(resp.statusCode).toEqual(400);
   });
 });
 
@@ -73,13 +73,40 @@ describe("test GET /items/:name", function() {
   });
 });
 
-// describe("test PATCH /items/:name", function() {
-//   it("updates single item", async function() {
-//     const resp = await request(app)
-//       .patch(`/items/${TEST_ITEM.name}`)
-//       .send()
-//   })
-// })
+describe("test PATCH /items/:name", function() {
+  it("updates single item", async function() {
+    const resp = await request(app)
+      .patch(`/items/${TEST_ITEM.name}`)
+      .send({ name: "new pretzel", price: 420});
+    expect(resp.statusCode).toEqual(200);
+    expect(resp.body).toEqual({updated: { name: "new pretzel", price: 420}});
+    expect(db.items[0]).toEqual({ name: "new pretzel", price: 420});
+  });
+
+  it("rejects invalid input patch requests", async function() {
+    const resp = await request(app)
+      .patch(`/items/${TEST_ITEM.name}`)
+      .send({
+        name: 'test',
+        price: 'string'
+      });
+    expect(resp.statusCode).toEqual(400);
+  });
+
+  it("rejects nonexistent item", async function() {
+    const resp = await request(app)
+      .patch("/items/nonexistent")
+      .send({ name: "new pretzel", price: 420});
+    expect(resp.statusCode).toEqual(404);
+  });
+
+  it("rejects empty body patch requests", async function() {
+    const resp = await request(app)
+      .patch(`/items/${TEST_ITEM.name}`)
+      .send({});
+    expect(resp.statusCode).toEqual(400);
+  });
+});
 
 describe("test DELETE /items/:name", function() {
   it("rejects nonexistent item", async function() {
